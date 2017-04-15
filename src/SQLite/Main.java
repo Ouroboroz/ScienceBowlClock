3f00c692c9dc4d80e72c0ebc085874e5f79d0a40
Kevin Yang <hbyang2000@gmail.com>
2017-04-15 15:24:55 -0400

    SQL jars

--- src/SQLite/Main.java
+++ src/SQLite/Main.java
@@ -2,6 +2,8 @@ package SQLite;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
  
@@ -21,15 +23,27 @@ public class Main {
         
         // SQL statement for creating a new table
         String sql = "CREATE TABLE IF NOT EXISTS questions (\n"
-                + "	id integer,\n"
-                + "	name text NOT NULL,\n"
-                + "	capacity real\n"
+                + "	round INTEGER NOT NULL,\n"
+                + "	'question number' INTEGER NOT NULL,\n"
+                + " question TEXT NOT NULL,\n"
+                + "	a TEXT, \n"
+                + " b TEXT, \n"
+                + " c TEXT, \n"
+                + " d TEXT, \n"
+                + " PRIMARY KEY(round, 'question number')\n"
                 + ");";
         
         try (Connection conn = DriverManager.getConnection(url);
                 Statement stmt = conn.createStatement()) {
             // create a new table
             stmt.execute(sql);
+            sql = "INSERT INTO question(round, 'question number', question, a, b, c, d) VALUES(?,?,?,?,?,?,?)";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString
+            sql = "SELECT * FROM questions;";
+            ResultSet set = stmt.executeQuery(sql);
+           	while(set.next())
+           		set.getString("question");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
